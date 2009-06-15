ac8e0b5254d7c058a2de54b868f21e84eef06596
scottlittlewood <scottlittlewood@ea01ec24-59f6-11de-b59c-372aa07dcf7f>
2009-06-15 21:59:55 +0000

    Initial SVN checkin of the simple dotnet multicast library

---
+++ Multicast/Multicast/IMulticastBroadcaster.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Multicast
+{
+    public interface IMulticastBroadcaster : IDisposable
+    {
+        MulticastSettings Settings { get; }
+
+        bool IsBound { get; }
+
+        void Broadcast(byte[] data);
+    }
+}
---
+++ Multicast/Multicast/IMulticastListener.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Multicast
+{
+    public delegate void ReceiveHandler(byte[] data);
+
+    public interface IMulticastListener : IDisposable
+    {
+        event ReceiveHandler OnReceive;
+
+        MulticastSettings Settings { get; }
+
+        bool IsBound { get; }
+
+        void StartListening(ReceiveHandler handler);
+        void StopListening();
+    }
+}
