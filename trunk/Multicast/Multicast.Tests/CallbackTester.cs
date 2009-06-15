a3aa782c9c6b81f63a38b9cc4e3c5a80505bfcd7
scottlittlewood <scottlittlewood@ea01ec24-59f6-11de-b59c-372aa07dcf7f>
2009-06-15 21:59:55 +0000

    Initial SVN checkin of the simple dotnet multicast library

---
+++ trunk/Multicast/Multicast.Tests/CallbackTester.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Multicast.Tests
+{
+    public class CallbackTester
+    {
+        public byte[] CallbackData { get; protected set; }
+
+        public CallbackTester()
+        {
+            CallbackData = new byte[0];
+        }
+
+        public void ReceiveCallback(byte[] data)
+        {
+            CallbackData = data;
+        }
+    }
+}
